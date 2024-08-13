aaef5a58dbfe2a02e06d56b13b008e72bc3e1ecf
PauloCesardev99 <paulocesarbritocamilo@gmail.com>
2024-08-13 18:35:09 -0300

    Criações de componente, style globais etc..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './_componentes/cabecalho/cabecalho.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SobreNosComponent } from './pages/home/_componentes/sobre-nos/sobre-nos.component';
+import { RodapeComponent } from './_componentes/rodape/rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabecalhoComponent,
+    HomeComponent,
+    SobreNosComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/home/_componentes/sobre-nos/sobre-nos.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sobre-nos',
+  templateUrl: './sobre-nos.component.html',
+  styleUrl: './sobre-nos.component.css'
+})
+export class SobreNosComponent {
+
+  paulo = {
+    nome: "Paulo",
+    sobre: "Programador Front End",
+    idade: 25,
+    cidade: "Tucuruvi-SP"
+  }
+
+  ana = {
+    nome: "Ana",
+    sobre: "Gestora de Ti",
+    idade: 22,
+    cidade: "Jardim Danfer-SP"
+  }
+
+}
